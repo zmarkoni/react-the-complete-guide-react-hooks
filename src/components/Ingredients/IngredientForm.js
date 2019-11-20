a7e3873e509f94c3b789899abf1f9f59290f4b35
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-11-20 10:04:19 +0100

    useState - addIngredientHandler

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -7,6 +7,7 @@ import './IngredientForm.css';
 const IngredientForm = React.memo(props => {
   //https://reactjs.org/docs/hooks-reference.html#usestate
 
+  // When we have one State for some connected properties
   // const [inputState, setInputState] = useState({
   //   title: '',
   //   amount: '' // We set Amount to string because VALUE of INPUT elements is always string
@@ -14,14 +15,18 @@ const IngredientForm = React.memo(props => {
 
   //useState will replace current state, not merge like before, so we need always to pass other state when using it.
   //Because of that, it is best to have state for each property independently!
+
+  //https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/15700336#announcements
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+      props.onAddIngredient({
+          id: Math.random().toString(),
+          title: enteredTitle,
+          amount: enteredAmount})
   };
-
 
   return (
     <section className="ingredient-form">
@@ -34,6 +39,14 @@ const IngredientForm = React.memo(props => {
                    onChange={event => {
                      setEnteredTitle(event.target.value);
                    }}
+                   // When we have one State for some connected properties
+                   // onChange={event => {
+                   //     const newAmount = event.target.value;
+                   //     setInputState(prevInputState => ({
+                   //         amount: newAmount,
+                   //         title: prevInputState.title
+                   //     }));
+                   // }}
              />
           </div>
           <div className="form-control">
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from "./IngredientList";
+const Ingredients = () => {
 
-function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+    const [ userIngredients, setUserIngredients ] = useState([]);
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
-}
+    const addIngredientHandler = (ingredient) => {
+        // We need to save STATE here and add new ingredients
+        setUserIngredients(prevIngredients => [...prevIngredients, {...ingredient}]);
+    };
+
+    return (
+        <div className="App">
+            {/* onAddIngredient will be passed to IngredientForm */}
+            <IngredientForm onAddIngredient={addIngredientHandler}/>
+
+            <section>
+                <Search/>
+                <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+            </section>
+        </div>
+    );
+};
 
 export default Ingredients;
