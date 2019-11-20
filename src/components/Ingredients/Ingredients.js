5bc0cfb0d7be97ed71bca037da8d92097d54553c
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-11-20 11:49:22 +0100

    useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,6 +7,31 @@ import IngredientList from "./IngredientList";
 const Ingredients = () => {
 
         const [userIngredients, setUserIngredients] = useState([]);
+
+        // We can use useEffect() for all sideEffects functionality.
+        // useEffect() is executed after and for EVERY render/rerender cycle
+        // useEffect accept 2 arguments
+        useEffect(() => {
+            fetch('https://react-hooks-84331.firebaseio.com/ingredients.json').then(
+                response => response.json()
+            ).then(responseData => {
+                const loadedIngredients = [];
+                for (const key in responseData) {
+                    loadedIngredients.push({
+                        id: key,
+                        title: responseData[key].title,
+                        amount: responseData[key].amount,
+                    });
+                }
+
+                // update state
+                setUserIngredients(loadedIngredients);
+            });
+        }, []); // with [] empty array as second argument, useEffect() will act like componentDidMount which runs ONLY ONCE(after the first render)!
+
+        useEffect(() => {
+            console.log('RENDERING INGREDIENTS', userIngredients);
+        }, [userIngredients]); // Will run when userIngredients change ONLY
 
         const addIngredientHandler = (ingredient) => {
             fetch('https://react-hooks-84331.firebaseio.com/ingredients.json', {
