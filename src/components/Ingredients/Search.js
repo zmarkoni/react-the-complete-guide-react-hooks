f8be65f5998f78f21f88cebdacc053eb484620b9
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-11-21 13:50:40 +0100

    cleanUp useEffect

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,11 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
-    const { onFilteredIngredients } = props; // object destructuring, so we can use it in useEffect dependencies
+    const {onFilteredIngredients} = props; // object destructuring, so we can use it in useEffect dependencies
     const [enteredFilter, setEnteredFilter] = useState('');
+    const inputRef = useRef();
 
     // We can use useEffect() for all sideEffects functionality.
     // useEffect() is executed after and for EVERY render/rerender cycle
@@ -13,41 +14,49 @@ const Search = React.memo(props => {
     useEffect(() => {
         // will execute right after onChange setEnteredFilter method
 
-        const queryFilter = enteredFilter.length === 0
-            ? ''
-            : `?equalTo="${enteredFilter}"&orderBy="title"`;
+        const timer = setTimeout(() => {
+            if (enteredFilter === inputRef.current.value) {
+                const queryFilter = enteredFilter.length === 0
+                    ? ''
+                    : `?equalTo="${enteredFilter}"&orderBy="title"`;
 
-        // To use this queryFilter we need to update Rules in Database to include ingredients.json
-        // {
-        //   "rules": {
-        //     ".read": true,
-        //     ".write": true,
-        //       "ingredients": { // added
-        //         ".indexOn": ["title"]
-        //       }
-        //   }
-        // }
+                // To use this queryFilter we need to update Rules in Database to include ingredients.json
+                // {
+                //   "rules": {
+                //     ".read": true,
+                //     ".write": true,
+                //       "ingredients": { // added
+                //         ".indexOn": ["title"]
+                //       }
+                //   }
+                // }
 
-        fetch('https://react-hooks-84331.firebaseio.com/ingredients.json' + queryFilter).then(
-            response => response.json()
-        ).then(responseData => {
-            const filteredIngredients = [];
-            for (const key in responseData) {
-                filteredIngredients.push({
-                    id: key,
-                    title: responseData[key].title,
-                    amount: responseData[key].amount,
+                fetch('https://react-hooks-84331.firebaseio.com/ingredients.json' + queryFilter).then(
+                    response => response.json()
+                ).then(responseData => {
+                    const filteredIngredients = [];
+                    for (const key in responseData) {
+                        filteredIngredients.push({
+                            id: key,
+                            title: responseData[key].title,
+                            amount: responseData[key].amount,
+                        });
+                    }
+
+                    // Pass filtered ingredients to Ingredients.js
+                    // props.onFilteredIngredients(filteredIngredients);
+                    onFilteredIngredients(filteredIngredients); // we can use without props since we have extract it in object destructuring on the top!!!
+
                 });
             }
+        }, 500);
 
-            // Pass filtered ingredients to Ingredients.js
-            // props.onFilteredIngredients(filteredIngredients);
-            onFilteredIngredients(filteredIngredients); // we can use without props since we have extract it in object destructuring on the top!!!
-
-        });
-    }, [enteredFilter, onFilteredIngredients]); // Will execute only when enteredFilter is changed
+        // useEffect can return cleanUp function and will run before new effect
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [enteredFilter, onFilteredIngredients, inputRef]); // Will execute only when enteredFilter is changed
     // with [] empty array as second argument, useEffect() will act like componentDidMount which runs ONLY ONCE(after the first render)!
-
     return (
         <section className="search">
             <Card>
@@ -55,6 +64,7 @@ const Search = React.memo(props => {
                     <label>Filter by Title</label>
                     <input type="text"
                            value={enteredFilter}
+                           ref={inputRef}
                            onChange={event => setEnteredFilter(event.target.value)}
                     />
                 </div>
