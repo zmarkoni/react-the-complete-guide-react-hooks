a87d6d1b6ffeb0ef8278ff2cf57afbafc828da9c
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-11-21 16:30:50 +0100

    error and loading handling

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,69 +1,74 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 // useState manage State in functional components
 import Card from '../UI/Card';
 import './IngredientForm.css';
+import '../UI/LoadingIndicator';
+import LoadingIndicator from "../UI/LoadingIndicator";
 
 // React.memo is used only to rerender component is PROPS are changed!
 const IngredientForm = React.memo(props => {
-  //https://reactjs.org/docs/hooks-reference.html#usestate
+    //https://reactjs.org/docs/hooks-reference.html#usestate
 
-  // When we have one State for some connected properties
-  // const [inputState, setInputState] = useState({
-  //   title: '',
-  //   amount: '' // We set Amount to string because VALUE of INPUT elements is always string
-  // });
+    // When we have one State for some connected properties
+    // const [inputState, setInputState] = useState({
+    //   title: '',
+    //   amount: '' // We set Amount to string because VALUE of INPUT elements is always string
+    // });
 
-  //useState will replace current state, not merge like before, so we need always to pass other state when using it.
-  //Because of that, it is best to have state for each property independently!
+    //useState will replace current state, not merge like before, so we need always to pass other state when using it.
+    //Because of that, it is best to have state for each property independently!
 
-  //https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/15700336#announcements
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
+    //https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/15700336#announcements
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
 
-  const submitHandler = event => {
-    event.preventDefault();
-      props.onAddIngredient({
-          title: enteredTitle,
-          amount: enteredAmount
-      });
-  };
+    const submitHandler = event => {
+        event.preventDefault();
+        props.onAddIngredient({
+            title: enteredTitle,
+            amount: enteredAmount
+        });
+    };
 
-  return (
-    <section className="ingredient-form">
-      <Card>
-        <form onSubmit={submitHandler}>
-          <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title"
-                   value={enteredTitle}
-                   onChange={event => {
-                     setEnteredTitle(event.target.value);
-                   }}
-                   // When we have one State for some connected properties
-                   // onChange={event => {
-                   //     const newAmount = event.target.value;
-                   //     setInputState(prevInputState => ({
-                   //         amount: newAmount,
-                   //         title: prevInputState.title
-                   //     }));
-                   // }}
-             />
-          </div>
-          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount"
-                   value={enteredAmount}
-                   onChange={event => {
-                     setEnteredAmount(event.target.value);
-                   }}/>
-          </div>
-          <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
-          </div>
-        </form>
-      </Card>
-    </section>
-  );
+    console.log('loading: ', props.loading);
+
+    return (
+        <section className="ingredient-form">
+            <Card>
+                <form onSubmit={submitHandler}>
+                    <div className="form-control">
+                        <label htmlFor="title">Name</label>
+                        <input type="text" id="title"
+                               value={enteredTitle}
+                               onChange={event => {
+                                   setEnteredTitle(event.target.value);
+                               }}
+                            // When we have one State for some connected properties
+                            // onChange={event => {
+                            //     const newAmount = event.target.value;
+                            //     setInputState(prevInputState => ({
+                            //         amount: newAmount,
+                            //         title: prevInputState.title
+                            //     }));
+                            // }}
+                        />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="amount">Amount</label>
+                        <input type="number" id="amount"
+                               value={enteredAmount}
+                               onChange={event => {
+                                   setEnteredAmount(event.target.value);
+                               }}/>
+                    </div>
+                    <div className="ingredient-form__actions">
+                        <button type="submit">Add Ingredient</button>
+                        {props.loading && <LoadingIndicator/> }
+                    </div>
+                </form>
+            </Card>
+        </section>
+    );
 });
 
 export default IngredientForm;
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,21 +3,26 @@ import React, { useState, useEffect, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 
 const Ingredients = () => {
 
         const [userIngredients, setUserIngredients] = useState([]);
+        const [isLoading, setIsLoading] = useState(false);
+        const [error, setError] = useState();
 
         useEffect(() => {
             console.log('RENDERING INGREDIENTS', userIngredients);
         }, [userIngredients]); // Will run when userIngredients change ONLY
 
         const addIngredientHandler = (ingredient) => {
+            setIsLoading(true);
             fetch('https://react-hooks-84331.firebaseio.com/ingredients.json', {
                 method: 'POST',
                 body: JSON.stringify(ingredient),
                 headers: {'Content-Type': 'application/json'}
             }).then(response => {
+                setIsLoading(false);
                 return response.json();
             }).then(responseData => {
                 // We need to save STATE here and add new ingredients
@@ -28,16 +33,24 @@ const Ingredients = () => {
                         ...ingredient
                     }
                 ]);
+            }).catch(error => {
+                setError('Something went wrong: ', error.message);
+                setIsLoading(false);
             });
         };
 
         const removeIngredientHandler = (ingredientId) => {
+            setIsLoading(true);
             fetch(`https://react-hooks-84331.firebaseio.com/ingredients/${ingredientId}.json`, {
                 method: 'DELETE',
             }).then(response => {
+                setIsLoading(false);
                 // We need to get STATE here and remove ingredient
                 // filter return new array
                 setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
+            }).catch(error => {
+               setError('Something went wrong: ', error.message);
+                setIsLoading(false);
             });
         };
 
@@ -45,10 +58,20 @@ const Ingredients = () => {
             setUserIngredients(filteredIngredients);
         }, []); // Here we don't pass useCallback dependency since setUserIngredients is useState method
 
+        const clearError = () => {
+            setError(null);
+        };
+
         return (
             <div className="App">
+
+                {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+
                 {/* onAddIngredient will be passed to IngredientForm */}
-                <IngredientForm onAddIngredient={addIngredientHandler}/>
+                <IngredientForm
+                    onAddIngredient={addIngredientHandler}
+                    loading={isLoading}
+                />
 
                 <section>
                     <Search onFilteredIngredients={filteredIngredientsHandler}/>
