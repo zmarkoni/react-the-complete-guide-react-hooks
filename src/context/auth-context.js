0281f0f955d004e912476e8500d58b67af72fc3e
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-11-30 20:49:22 +0100

    comments

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+// This is regular React component, no need to include anything than React to create context.
+// Only when we want to use context we need useContext hook.
+// Also we need to wrap our parent component with Provider which is created here.
+
+// useContext(MyContext) only lets you read the context and subscribe to its changes.
+// You still need a <MyContext.Provider> above in the tree to provide the value for this context.
 
 // We will subscribe on this context in App.js
 export const AuthContext = React.createContext({
