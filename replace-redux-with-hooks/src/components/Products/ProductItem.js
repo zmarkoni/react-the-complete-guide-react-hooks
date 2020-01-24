fb0f2773bc186f454036aa18bd6a619d920eec62
Zoran Markovic <zoran.markovic@netcentric.biz>
2020-01-24 15:54:47 +0100

    replace-redux-with-hooks replace Redux with context for Favorits and finish this module

--- replace-redux-with-hooks/src/components/Products/ProductItem.js
+++ replace-redux-with-hooks/src/components/Products/ProductItem.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 //import { useDispatch } from 'react-redux';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
+//import { toggleFav } from '../../store/actions/products';
+import { ProductsContext } from "../../context/products-context";
 
 const ProductItem = props => {
   //const dispatch = useDispatch();
 
+  const toggleFav = useContext(ProductsContext).toggleFav;
+
   const toggleFavHandler = () => {
     //dispatch(toggleFav(props.id));
+    toggleFav(props.id);
   };
 
   return (
--- replace-redux-with-hooks/src/context/products-context.js
+++ replace-redux-with-hooks/src/context/products-context.js
@@ -1,42 +1,66 @@
-import React, { useState} from 'react';
+import React, {useState} from 'react';
 
 // use Context instead of REDUX
 // So we will set the store here in ProductsContext
 
-export const ProductsContext = React.createContext({products: []});
+export const ProductsContext = React.createContext({
+    products: [],
+    toggleFav: (id) => {}
+});
 
 export default props => {
-    // copy initialState from reducer
+
+    // copy everything from REDUCER
+
+    // initialState from reducer
     const [productsList, setProductsList] = useState([
         {
-          id: 'p1',
-          title: 'Red Scarf',
-          description: 'A pretty red scarf.',
-          isFavorite: false
+            id: 'p1',
+            title: 'Red Scarf',
+            description: 'A pretty red scarf.',
+            isFavorite: false
         },
         {
-          id: 'p2',
-          title: 'Blue T-Shirt',
-          description: 'A pretty blue t-shirt.',
-          isFavorite: false
+            id: 'p2',
+            title: 'Blue T-Shirt',
+            description: 'A pretty blue t-shirt.',
+            isFavorite: false
         },
         {
-          id: 'p3',
-          title: 'Green Trousers',
-          description: 'A pair of lightly green trousers.',
-          isFavorite: false
+            id: 'p3',
+            title: 'Green Trousers',
+            description: 'A pair of lightly green trousers.',
+            isFavorite: false
         },
         {
-          id: 'p4',
-          title: 'Orange Hat',
-          description: 'Street style! An orange hat.',
-          isFavorite: false
+            id: 'p4',
+            title: 'Orange Hat',
+            description: 'Street style! An orange hat.',
+            isFavorite: false
         }
-      ]);
+    ]);
 
-  return (
-    <ProductsContext.Provider value={{products: productsList}}>
-        {props.children}
-    </ProductsContext.Provider>
-  );
+    const toggleFavorite = productId => {
+        // setProductsList from useState
+        setProductsList(currentProductList => {
+            const prodIndex = currentProductList.findIndex(
+                p => p.id === productId
+            );
+            const newFavStatus = !currentProductList[prodIndex].isFavorite;
+            const updatedProducts = [...currentProductList];
+
+            updatedProducts[prodIndex] = {
+                ...currentProductList[prodIndex],
+                isFavorite: newFavStatus
+            };
+
+            return updatedProducts;
+        });
+    };
+
+    return (
+        <ProductsContext.Provider value={{products: productsList, toggleFav: toggleFavorite}}>
+            {props.children}
+        </ProductsContext.Provider>
+    );
 };
